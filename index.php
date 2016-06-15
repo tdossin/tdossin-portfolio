ff0e554277e5bbbf2a010ae80946e505904cd706
Toby Dossin <tdossin@gmail.com>
2016-06-14 20:25:39 -0400

    init

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Toby Dossin - Web Developer</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6 col-xs-12">
+				<h1>Toby Dossin</h1>
+				tobydossin.com<br/>
+				tdossin[at]gmail.com<br/>
+				813.297.8869
+			</div>
+			<div class="col-sm-6 col-xs-12 goal right">
+
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 section">
+				<h2>Technical Skills</h2>
+			</div>
+			<div class="col-xs-2 skillset">
+				Languages
+			</div>
+			<div class="col-sm-6 col-xs-12">
+				HTML5, CSS, Javascript, PHP
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-2 skillset">
+				Frameworks
+			</div>
+			<div class="col-sm-6 col-xs-12">
+				jQuery, AngularJS, Bootstrap, Wordpress, Joomla, Magento
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-2 skillset">
+				Software 
+			</div>
+			<div class="col-sm-6 col-xs-12">
+				Visual Studio, Adobe Illustrator, Photoshop, Premier, After Effects
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-2 skillset">
+				Technology
+			</div>
+			<div class="col-sm-6 col-xs-12">
+				APIs, MVC, AJAX, REST, JSON, XML, Agile Process
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 section">
+				<h2>Work Experience</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-6 bold">
+				Fleetistics
+			</div>
+			<div class="col-xs-6 bold right">
+				Wesley Chapel, FL
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-6 italic">
+				Director of Marketing - Sr. Web Developer
+			</div>
+			<div class="col-xs-6 right italic">
+				10/2012-6/2016
+			</div>
+		</div>
+		<div class="row">
+			<ul>
+				<li>Coordinated with back-end dev team to incorporate service modules and dashboards into a custom, responsive GPS  and telematics web portal</li>
+				<li>Created service modules for functions such as data visualization, account maintenance, and device management</li>
+				<li>Wireframed, prototyped, and developed RESTful services to optimize user experience, responsiveness, and scalability</li>
+				<li>Reduced Marketing budget 75% by eliminating the need to purchase online advertising spend by optimizing website for organic search results</li>
+				<li>Increased website traffic by 230% through site consolidation, streamlining, and SEO optimization</li>
+				<li>Grew web-based lead conversion rate by 58% for 2015 over 2014</li>
+				<li>Established project milestones for development work and consistently remained under budget and within scope</li>
+				<li>Spearheaded the company's rebranding and branded subsequent products and services as they were developed</li>
+				<li>Oversaw and assisted in the creation of advertising campaigns and landing pages</li>
+				<li>Tracked web statistics with Google analytics, webmaster tools, and heat-mapping software to make sure content was being accessed</li>
+				<li>Produced marketing materials such as brochures, postcards and tradeshow displays</li>
+				<li>Secured and managed partnerships with vendors</li>
+				<li>Conducted interviews and employee evaluations</li>
+			</ul>
+		</div>
+		<div class="row">
+			<div class="col-xs-6 italic">
+				Web Developer
+			</div>
+			<div class="col-xs-6 right italic">
+				1/2012-10/2012
+			</div>
+		</div>
+		<div class="row">
+			<ul>
+				<li>Architected, designed, and developed responsive websites and landing pages to maximize SEO</li>
+				<li>Spearheaded the rebranding effort of the entire company to update visuals, designs, and website</li>
+				<li>Consistently completed project work within established timelines and scope</li>
+				<li>Strong in front-end GUI development using HTML, CSS, and Javascript</li>
+			</ul>
+		</div>
+		<div class="row">
+			<div class="col-xs-6 bold">
+				Unknown.com, Inc. / Ignotus Games
+			</div>
+			<div class="col-xs-6 bold right">
+				Tampa, FL
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-6 italic">
+				Project Lead/Web Administrator
+			</div>
+			<div class="col-xs-6 right italic">
+				3/2010 to 1/2012
+			</div>
+		</div>
+		<div class="row">
+			<ul>
+				<li>Rebranded, restructured and rolled-out improved company website in accordance with overall company strategy and a focus on core competencies</li>
+				<li>Provided assistance with graphics, content and marketing strategies as needed for mobile apps to ensure their market relevancy, proper functionality, and timely delivery</li>
+				<li>Designed and maintained a web presence to showcase their latest iOS game</li>
+				<li>Managed a team tasked with organizing, co-authoring and copy-editing a book on iOS Development</li>
+				<li>Conducted team meetings, outlined goals and ensured prompt delivery of necessary assets</li>
+				<li>Performed payroll duties, sought out, hired and trained new employees</li>
+			</ul>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 section">
+				<h2>Formal Education</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12">
+				B.F.A - Web Design and Interactive Media - <span class="italic">Art Institute of Tampa</span>
+			</div>
+			<div class="col-xs-12">			
+				B.S. - Business Administration Marketing - <span class="italic">Old Dominion University</span>
+			</div>
+		</div>
+		<div class="row footer">
+		
+		</div>
+	</div><!--/container-->
+  </body>
+</html>
